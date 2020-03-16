b9e1662f4737cf2852f6b24eff3cc77537a9c2b8
PhMueller <muller-phil@gmx.net>
2020-03-16 17:37:29 +0100

    [skip travis] update

--- codestyle.sh
+++ codestyle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [[ "$RUN_CODESTYLE" == "true" ]]; then
-  pip install .[codestyle]
+  pip install pycodestyle flake8
   pycodestyle --max-line-length=120 ./hpolib
   flake8 --max-line-length=120 ./hpolib
 fi
