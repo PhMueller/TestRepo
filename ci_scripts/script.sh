5f227f972b815ecee15525d2b21062330c831c46
PhMueller <muller-phil@gmx.net>
2020-03-16 22:43:42 +0100

    update

--- ci_scripts/script.sh
+++ ci_scripts/script.sh
@@ -2,8 +2,8 @@
 
 if [[ "$USE_SINGULARTIY" == "true" ]]; then
     # Create the coverage report for the singularity example, since it covers more tests.
-    pytest --cov=hpolib
+    pytest -sv --cov=hpolib tests/
     codecov --token=$CODECOV_TOKEN
 else
-    pytest
+    pytest -sv
 fi
