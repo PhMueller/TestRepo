0264d2799a4c318afff18f58039fc91676e075c4
PhMueller <muller-phil@gmx.net>
2020-03-16 17:54:48 +0100

    update

--- ci_scripts/codestyle.sh
+++ ci_scripts/codestyle.sh
@@ -2,6 +2,8 @@
 
 if [[ "$RUN_CODESTYLE" == "true" ]]; then
   pip install pycodestyle flake8
+  pwd
+  echo "$(pwd) Curr directory"
   pycodestyle --max-line-length=120 ./hpolib
   flake8 --max-line-length=120 ./hpolib
 fi
---
+++ ci_scripts/install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+if [[ "$USE_SINGULARITY" == "true" ]]; then
+    echo "USE SINGULARTIY "
+
+    sudo apt-get update && sudo apt-get install -y \
+      build-essential \
+      libssl-dev \
+      uuid-dev \
+      libgpgme11-dev \
+      squashfs-tools \
+      libseccomp-dev \
+      wget \
+      pkg-config \
+      git \
+      cryptsetup
+
+    export VERSION=1.13 OS=linux ARCH=amd64 && \  # Replace the values as needed
+      wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \ # Downloads the required Go package
+      sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \ # Extracts the archive
+      rm go$VERSION.$OS-$ARCH.tar.gz    # Deletes the ``tar`` file
+
+    echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
+      source ~/.bashrc
+
+    export VERSION=3.5.2 && # adjust this as necessary \
+      wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
+      tar -xzf singularity-${VERSION}.tar.gz && \
+      cd singularity
+
+    ./mconfig && \
+      make -C builddir && \
+      sudo make -C builddir install
+
+    singularity help
+
+    pip install .[xgboost,singularity]
+else
+    echo "DONT USE SINGULARTIY "
+    pip install .[xgboost]
+fi
+
