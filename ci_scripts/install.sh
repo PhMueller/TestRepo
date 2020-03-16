c4a60a720f6a5f492fdaba020d12cd57a794f2da
PhMueller <muller-phil@gmx.net>
2020-03-16 18:41:21 +0100

    update

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -19,13 +19,13 @@ if [[ "$USE_SINGULARITY" == "true" ]]; then
       git \
       cryptsetup
 
-    export VERSION=1.13 OS=linux ARCH=amd64 && \  # Replace the values as needed
-      wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \ # Downloads the required Go package
-      sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \ # Extracts the archive
-      rm go$VERSION.$OS-$ARCH.tar.gz    # Deletes the ``tar`` file
+    # export VERSION=1.13 OS=linux ARCH=amd64 && \  # Replace the values as needed
+    #   wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \ # Downloads the required Go package
+    #   sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \ # Extracts the archive
+    #   rm go$VERSION.$OS-$ARCH.tar.gz    # Deletes the ``tar`` file
 
-    echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
-      source ~/.bashrc
+    # echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
+    #   source ~/.bashrc
 
     export VERSION=3.5.2 && # adjust this as necessary \
       wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
